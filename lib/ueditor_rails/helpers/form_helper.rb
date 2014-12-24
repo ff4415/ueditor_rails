e95c397ba9ee9e7dfcd7db2b4bc916ee36dbb370
chinacheng <chinachengliwen@gmail.com>
2014-12-24 14:44:58 +0800

    active view tags and assetts in rails4

--- lib/ueditor_rails/helpers/form_helper.rb
+++ lib/ueditor_rails/helpers/form_helper.rb
@@ -5,9 +5,15 @@ module UeditorRails
 
       include ActionView::Helpers::TagHelper
       include ActionView::Helpers::JavaScriptHelper
+      include ActionView::Helpers::Tags
 
       def ueditor_text(object_name, method = nil, options = {})
-        instance_tag = ActionView::Helpers::ActiveModelInstanceTag.content_tag(object_name, method, self, options.delete(:object))
+        if defined?(ActionView::Base::InstanceTag)
+          instance_tag = ActionView::Base::InstanceTag.new(object_name, method, self, options.delete('object'))
+        else
+          instance_tag = ActionView::Helpers::Tags::TextArea.new(object_name, method, self, options)
+        end
+        # instance_tag = ActionView::Helpers::Tags::TextArea.new(object_name, method, self, options)
         instance_tag.send(:add_default_name_and_id, options) if options[:id].blank?
 
         element_id = options.delete('id')
@@ -17,7 +23,12 @@ module UeditorRails
 
         output_buffer = ActiveSupport::SafeBuffer.new
         #output_buffer << instance_tag.to_content_tag(:script, ue_tag_attributes)
-        output_buffer << instance_tag.to_text_area_tag(ue_tag_attributes)
+        if defined?(ActionView::Base::InstanceTag)
+          output_buffer << instance_tag.to_text_area_tag(ue_tag_attributes)
+        else
+          output_buffer << instance_tag.render
+        end
+        # output_buffer << instance_tag.to_text_area_tag(ue_tag_attributes)
         output_buffer << javascript_tag {Util.js_replace(element_id, options.stringify_keys)}
         output_buffer
       end
